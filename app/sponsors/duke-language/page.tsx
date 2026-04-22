02f49f531d1a025c2da1a8d18a4f6bb01d7bb9fa
Davidjohnray <your-email@example.comraydave8@hotmail.com>
2026-04-22 20:51:43 +0700

    fix homepage and add duke language redirect page

---
+++ app/sponsors/duke-language/page.tsx
@@ -0,0 +1,6 @@
+export const dynamic = 'force-dynamic'
+import { redirect } from 'next/navigation'
+
+export default function DukeLanguageRedirect() {
+  redirect('https://dukelanguage.com/?utm_source=jobsinthailand&utm_medium=banner&utm_campaign=sidebar')
+}
