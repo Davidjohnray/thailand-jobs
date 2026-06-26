9984393d657324de92f21719bc3b47b47473fd96
Davidjohnray <your-email@example.comraydave8@hotmail.com>
2026-06-26 14:55:14 +0700

    delete sw.js to fix service worker crash on post-featured page

--- public/sw.js
+++
@@ -1,5 +0,0 @@
-self.addEventListener('install', () => self.skipWaiting())
-self.addEventListener('activate', () => {})
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
-})
