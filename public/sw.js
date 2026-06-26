37198375be61bd0381600996e0b743f00c606715
Davidjohnray <your-email@example.comraydave8@hotmail.com>
2026-06-26 14:48:55 +0700

    fix service worker crash

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch', function(event) {});
+self.addEventListener('install', () => self.skipWaiting())
+self.addEventListener('activate', () => {})
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting()
+})
